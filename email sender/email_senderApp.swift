93588ee2ec75d514121c2467a6f15711dffb7428
krzysiekk22d <klas.club@gmail.com>
2023-12-23 22:26:37 +0100

    Initial Commit

---
+++ email sender/email_senderApp.swift
@@ -0,0 +1,17 @@
+//
+//  email_senderApp.swift
+//  email sender
+//
+//  Created by Krzysztof Czura on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct email_senderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
