218c66d1142dc7120cb721cdd2344f273fb7b370
krzysiekk22d <klas.club@gmail.com>
2023-12-24 03:05:43 +0100

    Finish the project.

---
+++ email sender/MailView.swift
@@ -0,0 +1,18 @@
+//
+//  MailView.swift
+//  email sender
+//
+//  Created by Krzysztof Czura on 24/12/2023.
+//
+
+import SwiftUI
+
+struct MailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MailView()
+}
